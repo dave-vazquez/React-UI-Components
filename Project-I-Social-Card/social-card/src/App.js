d667ea9b287a8e689aa87a17d2105754ae8d1f3f
dmvazli <davidmvazquez1@gmail.com>
2019-05-11 18:26:10 -0400

    cleaned up css

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -15,3 +15,14 @@ const App = () => {
 };
 
 export default App;
+
+/* 
+
+  Personal Goals:
+
+  1) Run current version of CRA remotely, copy project files over and refactor styles using LESS or SASS
+  2) Make responsive with LESS, SASS or possibly bootstrap.
+  3) Fine another library for the icons.
+  3) Make share and like icons increment the count.
+
+*/
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -5,6 +5,7 @@ const CardBanner = () => {
   return (
     <div className="card-banner-container">
       <img
+        className="card-banner"
         src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
         alt="react logo"
       />
