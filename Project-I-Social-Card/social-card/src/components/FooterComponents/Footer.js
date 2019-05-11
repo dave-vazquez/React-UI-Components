45cf454e5b7b6b8251d8bfa736299858e320943b
dmvazli <davidmvazquez1@gmail.com>
2019-05-11 01:15:57 -0400

    added all unstyled components

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+export const Footer = () => {
+    return (
+        <div>
+            <i class="far fa-comment"></i>
+            <i class="fas fa-sync-alt"></i> 6
+            <i class="far fa-heart"></i> 4
+            <i class="far fa-envelope"></i>
+        </div>
+    );
+}
+
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title-container">
+            <h1>Lambda School</h1>
+            <p>@Lambda School · 11 may</p>
+        </div>
+
+    );
+}
+
+export default HeaderTitle;
